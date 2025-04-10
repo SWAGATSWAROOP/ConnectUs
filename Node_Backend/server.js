b5a2b53ec679d982b071a40228d158061800a031
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2025-04-10 18:25:51 +0530

    Changed some files

--- Node_Backend/server.js
+++ Node_Backend/server.js
@@ -1,20 +1,18 @@
-
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
 
 dotenv.config();
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
